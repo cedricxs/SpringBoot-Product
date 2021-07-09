941ad10771b5384c3592b49c3d6eef380100cbd4
chaxingshuo.cxs <chaxingshuo.cxs@alibaba-inc.com>
2021-07-10 01:12:52 +0800

    add router

--- src/main/java/com/cedricxs/application/controller/MainController.java
+++ src/main/java/com/cedricxs/application/controller/MainController.java
@@ -1,13 +1,29 @@
 package com.cedricxs.application.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+/**
+ * @author chaxingshuo
+ * @date 2021/07/10
+ */
+@Controller
 public class MainController {
 
-    @GetMapping("/")
-    public String sayHello() {
-        return "hello cedricxs!";
+    @GetMapping({"/", "/index"})
+    public String index() {
+        return "index.html";
     }
+
+    @GetMapping("/elements")
+    public String elements() {
+        return "elements.html";
+    }
+
+    @GetMapping("/generic")
+    public String generic() {
+        return "generic.html";
+    }
+
+
 }
